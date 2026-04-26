4764a753d5140cbe19794ab83d6a43f0577a8785
Developer-Choi-Jae-Young <77819208+Developer-Choi-Jae-Young@users.noreply.github.com>
2026-04-26 20:33:15 +0900

    R2 추가

---
+++ src/main/java/org/example/infratest/config/S3Config.java
@@ -0,0 +1,36 @@
+package org.example.infratest.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.s3.endpointUrl}")
+    private String s3EndpointUrl;
+
+    @Value("${aws.accessKey}")
+    private String awsAccessKey;
+
+    @Value("${aws.secretKey}")
+    private String awsSecretKey;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsCredentials credentials = AwsBasicCredentials.create(awsAccessKey, awsSecretKey);
+
+        return S3Client.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .region(Region.of("auto"))
+                .endpointOverride(URI.create(s3EndpointUrl))
+                .build();
+    }
+}
--- src/main/java/org/example/infratest/controller/MainController.java
+++ src/main/java/org/example/infratest/controller/MainController.java
@@ -1,15 +1,22 @@
 package org.example.infratest.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.infratest.service.FileUploadService;
 import org.example.infratest.service.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/main")
 public class MainController {
     private final MemberService memberService;
+    private final FileUploadService fileUploadService;
 
     @PostMapping("/save")
     public ResponseEntity<?> save(@RequestBody String name) {
@@ -19,5 +26,11 @@ public class MainController {
     @GetMapping("/list")
     public ResponseEntity<?> list() {
         return ResponseEntity.ok(memberService.list());
+    }
+
+    @PostMapping(path = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        String url = fileUploadService.uploadImage(file);
+        return ResponseEntity.status(HttpStatus.CREATED).body(url);
     }
 }
