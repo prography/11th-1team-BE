87fa5eab791b852578ec916de589d633b2346434
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-04-30 19:50:11 +0900

    chore: 중복 파일 제거

--- src/main/java/org/example/infratest/global/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package org.example.infratest.global;
-
-import org.springframework.http.HttpStatus;
-
-public interface ErrorCode {
-    String name();
-    HttpStatus getHttpStatus();
-    String getMessage();
-}
