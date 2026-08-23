40b6d93b4a89a176a82e998d9bb24cd44221b7a1
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-08-23 20:16:14 +0900

    chore: 시드데이터 운영에서 off

--- src/main/java/org/example/knockin/config/SeedDataConfig.java
+++ src/main/java/org/example/knockin/config/SeedDataConfig.java
@@ -158,7 +158,7 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
-@Profile("prod")
+@Profile("!prod")
 public class SeedDataConfig implements CommandLineRunner {
     private final AgreementRepository agreementRepository;
     private final AgreementLogRepository agreementLogRepository;
